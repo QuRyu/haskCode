8d4c8f7366475814d79265a774072614318d7d0c
QuRyu <Quentin.Liu.0415@gmail.com>
2018-03-04 23:02:35 -0500

    added profile info

--- pcap/app/Main.hs
+++ pcap/app/Main.hs
@@ -1,6 +1,36 @@
 module Main where
 
+import System.Environment 
+import Control.Monad 
+
+import qualified Data.ByteString.Lazy as BL
+import Data.Binary.Get  
+
 import Lib
 
-main :: IO ()
-main = someFunc
+main :: IO () 
+main = do 
+    args <- getArgs 
+    let ordering = "-r" `elem` args 
+    let args' = act ordering removeR args 
+    if null args' || length args' > 1 
+        then putStrLn "Expecting one argument!"
+        else do pcap <- readPcap args' 
+                let mData = act ordering sortMarketData (getMarketData pcap)
+                forM_ mData print 
+        
+
+  where 
+    act True f = f  
+    act _    _ = id 
+
+    removeR [] = [] 
+    removeR (x:xs) | x == "-r" = xs
+                   | otherwise = x : removeR xs 
+
+readPcap :: [String] -> IO Pcap 
+readPcap (path:_) = do
+    content <- BL.readFile path 
+    return $ parsePCAP content 
+                
+    
--- pcap/src/Lib.hs
+++ pcap/src/Lib.hs
@@ -1,6 +1,29 @@
 module Lib
-    ( someFunc
+    ( PGlobalHeader
+    , Transaction 
+    , MarketData 
+    , Pcap 
+
+    , parsePCAP
+    , getMarketData 
+    , accTime 
+    , sortMarketData 
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+
+import Data.List 
+
+import Data.Binary.Get 
+import Data.Text.Encoding
+import qualified Data.Text as Text
+import qualified Data.ByteString.Lazy as BL
+
+import PcapParser hiding (parsePCAP) 
+import qualified PcapParser as PCAP (parsePCAP)
+
+parsePCAP :: BL.ByteString -> Pcap  
+parsePCAP = runGet PCAP.parsePCAP 
+
+sortMarketData :: [MarketData] -> [MarketData] 
+sortMarketData = sort 
+
